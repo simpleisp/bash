7531454801d27e5935cab1560884438a7d83978d
simpleisp <131302176+simpleisp@users.noreply.github.com>
2023-09-03 11:33:56 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Install the application
 sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y
+sudo apt-get upgrade -y
 chmod +x install.sh
 sed -i -e 's/\r$//' install.sh
-./install.sh < /dev/null
+./install.sh
